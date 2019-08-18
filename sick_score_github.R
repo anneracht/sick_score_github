b064c25c99235696c2d11b56b05396472781add9
anneracht <anneracht@gmail.com>
2019-08-18 10:53:54 -0400

    notebook added with references compiled in notebook

--- sick_score_github.R
+++ sick_score_github.R
@@ -2,9 +2,10 @@ getwd()
 library(dplyr)
 require(gdata)
 
-
+#Pull data from desktop folder
 df = read.xls(("sick_score_xls.xls"), sheet = 1, header = TRUE)
 sick_score_original <- rename(df)
 head(sick_score)
 
+#convert variables to diff names
 
