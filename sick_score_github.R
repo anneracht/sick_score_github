62a32a301df66b6bf57f5af78ee193b09c431e40
anneracht <anneracht@gmail.com>
2019-08-18 11:14:00 -0400

    101

--- sick_score_github.R
+++ sick_score_github.R
@@ -1,5 +1,23 @@
 getwd()
 library(dplyr)
+library(tibble)
+library(tidyr)
+library(magrittr)
+library(ggplot2)
+library(reshape2)
+library(igraph)
+library(DiagrammeR)
+library(tm)
+library(base)
+library(stringr)
+library(syuzhet) # for Anne sentiment experiment
+library(RColorBrewer)
+library(wordcloud)
+library(dplyr)
+library(purrr)
+library(tidyr)
+library(stringr)
+
 require(gdata)
 
 #Pull data from desktop folder
@@ -7,5 +25,6 @@ df = read.xls(("sick_score_xls.xls"), sheet = 1, header = TRUE)
 sick_score_original <- rename(df)
 head(sick_score)
 
-#convert variables to diff names
+#convert variables to
+
 
