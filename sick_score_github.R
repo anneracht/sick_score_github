2596be6b5d5bfb4b49adb19b095eab35043ac0f7
anneracht <anneracht@gmail.com>
2019-08-18 13:43:31 -0400

    - identifying empty datapoints and converted them to NA - abbreviated Race - basic distribution of race and age

--- sick_score_github.R
+++ sick_score_github.R
@@ -24,7 +24,12 @@ require(gdata)
 df = read.xls(("sick_score_xls.xls"), sheet = 1, header = TRUE)
 sick_score_original <- rename(df)
 head(sick_score)
-
-#convert variables to
+#open up the spreadsheet on R
+view(sick_score)
+# Inspecting data. View the no. of variables and levels for each variable.
+sick_score %>% tbl_vars()
+str(sick_score)
+#total variables 49. I'll modify the CKD , CAD, CHF, CVA, liver data to reduce it to 1 variable column.
+#but first, I'll convert the obvious binaries into 1 and 0 . The edited spreadsheet is still named sick_score for simplicity purpose.
 
 
